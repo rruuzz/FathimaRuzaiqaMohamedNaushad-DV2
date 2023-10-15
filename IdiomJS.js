b1b48cbb07e6cb76a3042f2250d5ba0e2f605d84
rruuzz <fmoh0037@student.monash.edu>
2023-10-15 16:52:46 +0800

    change 4

--- IdiomJS.js
+++ IdiomJS.js
@@ -1,11 +1,11 @@
-vegaEmbed('#chart1', "Idioms/Bar01.json").then(function (result) {
+vegaEmbed('chart1', "Idioms/Bar01.json").then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed('#chart2', "Idioms/Bubble.json").then(function (result) {
+vegaEmbed('chart2', "Idioms/Bubble.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart3', "Idioms/Map.json").then(function (result) {
+vegaEmbed('chart3', "Idioms/Map.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart4', "Idioms/Scatter01.json").then(function (result) {
+vegaEmbed('chart4', "Idioms/Scatter01.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart5', "Scatter02.json").then(function (result) {
+vegaEmbed('chart5', "Scatter02.json").then(function (result) {
 }).catch(console.error);
